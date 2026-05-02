129c479c777d06ac41f090cb5d8d85a868771e51
Pablo Ricardo <143361828+pablo891@users.noreply.github.com>
2026-05-01 21:01:48 -0300

    Estudando APIs Rest Spring em Java

---
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -0,0 +1,32 @@
+package med.voll.api.paciente;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import med.voll.api.endereco.Endereco;
+
+
+@Getter
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "Paciente")
+@Table(name = "pacientes")
+
+public class Paciente {
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String cpf;
+    private String telefone;
+
+    @Embedded
+    private Endereco endereco;
+
+    public Paciente(DadosCadastroPaciente dados){
+    }
+
+}
---
+++ api/src/main/java/med/voll/api/paciente/PacienteController.java
@@ -0,0 +1,32 @@
+package med.voll.api.paciente;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Medico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("pacientes")
+public class PacienteController {
+
+    @Autowired
+    private PacienteRepository repository;
+
+    @PostMapping
+    @Transactional
+    public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
+        repository.save(new Paciente(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemPaciente> listar(@PageableDefault(page = 0, size = 10, sort = {"nome"}) Pageable paginacao) {
+        return repository.findAll(paginacao).map(DadosListagemPaciente::new);
+    }
+}
